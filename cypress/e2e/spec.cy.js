6cc25873a8f9294c75d47f81ebcd222e8ec81249
Hugo Myrberg <hugo.myrberg05@skola.stenungsund.se>
2025-01-31 10:32:20 +0100

    new

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,36 +1,13 @@
-// Import cypress-axe
+
+
+
 
 import 'cypress-axe';
- 
-describe('Accessibility Testing', () => {
 
-  beforeEach(() => {
-
-    // Visit the page you want to test
-
+describe('Testar Cypress', () => {
+  it('Besöker en webbsida', () => {
     cy.visit('https://hugomyrberg.github.io/first-pwa/');
-
-    // Inject the Axe-core library into the page
-
-    cy.injectAxe();
-
   });
- 
-  it('should have no detectable accessibility violations on load', () => {
-
-    // Run the accessibility checks using Axe-core
-
-    cy.checkA11y(null, null, (violations) => {
-
-      if (violations.length) {
-
-        throw new Error(`Accessibility issues found: ${violations.length}`);
-
-      }
-
-    });
-
-  });
-
 });
+
  
