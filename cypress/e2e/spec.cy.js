46b1be9a57f88649f4ad7feacfd13ed26cea72f0
Hugo Myrberg <hugo.myrberg05@skola.stenungsund.se>
2025-01-31 11:52:44 +0100

    new

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,10 +4,13 @@
 
 import 'cypress-axe';
 
+
 describe('Testar Cypress', () => {
   it('Besöker en webbsida', () => {
     cy.visit('https://hugomyrberg.github.io/first-pwa/');
+    cy.injectAxe(); 
+    cy.checkA11y();
   });
 });
 
- 
+
